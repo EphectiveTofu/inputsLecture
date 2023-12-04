0c38b7076b433db13238d6cff4b0c1a9542273d7
Ken <kmartinez@sjcoe.net>
2023-12-04 13:47:37 -0800

    Commit input lecture

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+// create the variables for the elements
+let myInput = document.getElementById("myInput");
+let submitBtn = document.getElementById("submitBtn");
+let displayBtn = document.getElementById("displayBtn");
+
+let inputDisplay = document.getElementById("inputDisplay");
+
+let savedInput;
+
+submitBtn.addEventListener("click", function(event){
+    savedInput = myInput.value;
+    console.log(savedInput);
+    
+});
+
+displayBtn.addEventListener("click", function(event){
+    inputDisplay.className = "designed";    
+    inputDisplay.innerText = savedInput;
+});
